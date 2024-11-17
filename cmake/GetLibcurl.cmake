f16eb9ba0d14611d040e22570cecc44f03cda659
Samuel Divine <divinesamuel1515@gmail.com>
2024-11-17 19:24:43 +0800

    Update: cmake get modules

--- cmake/GetLibcurl.cmake
+++ cmake/GetLibcurl.cmake
@@ -5,7 +5,8 @@ set(CURL_USE_SCHANNEL ON)
 set(CURL_USE_OPENSSL OFF)
 set(USE_WINDOWS_SSPI ON)
 set(BUILD_CURL_EXE OFF)
-include(FetchContent)
+
+#[=[include(FetchContent)
 FetchContent_Declare(
     curl
     GIT_REPOSITORY https://github.com/curl/curl.git
@@ -14,7 +15,21 @@ FetchContent_Declare(
     GIT_SHALLOW TRUE
     DOWNLOAD_EXTRACT_TIMESTAMP FALSE
     EXCLUDE_FROM_ALL
+)]=]
+message(STATUS "Downloading and extracting curl library sources. This will take some time...")
+include(FetchContent)
+Set(FETCHCONTENT_QUIET FALSE) # Needed to print downloading progress
+FetchContent_Declare(
+    curl
+    URL https://github.com/curl/curl/releases/download/curl-8_9_1/curl-8.9.1.zip # downloading a zip release speeds up the download
+    USES_TERMINAL_DOWNLOAD TRUE
+    GIT_PROGRESS TRUE
+
+    # DOWNLOAD_NO_EXTRACT FALSE
+    DOWNLOAD_EXTRACT_TIMESTAMP FALSE
+    EXCLUDE_FROM_ALL
 )
+
 FetchContent_MakeAvailable(curl)
 
 # find_package(CURL REQUIRED)
--- cmake/GetSoci.cmake
+++ cmake/GetSoci.cmake
@@ -1,11 +1,11 @@
-set(BUILD_STATIC_LIBS ON)
-set(BUILD_SHARED_LIBS OFF)
+set(BUILD_STATIC_LIBS OFF)
+set(BUILD_SHARED_LIBS ON)
 set(SOCI_SHARED ON)
-set(SOCI_STATIC ON)
+set(SOCI_STATIC OFF)
 set(WITH_MYSQL ON)
 set(WITH_BOOST OFF)
 
-include(FetchContent)
+#[=[include(FetchContent)
 FetchContent_Declare(
     soci
     GIT_REPOSITORY https://github.com/SOCI/soci.git
@@ -14,5 +14,19 @@ FetchContent_Declare(
     GIT_SHALLOW TRUE
     DOWNLOAD_EXTRACT_TIMESTAMP FALSE
     EXCLUDE_FROM_ALL
+)]=]
+message(STATUS "Downloading and extracting soci library sources. This will take some time...")
+include(FetchContent)
+Set(FETCHCONTENT_QUIET FALSE) # Needed to print downloading progress
+FetchContent_Declare(
+    soci
+    URL https://github.com/SOCI/soci/archive/refs/tags/v4.0.3.zip # downloading a zip release speeds up the download
+    USES_TERMINAL_DOWNLOAD TRUE
+    GIT_PROGRESS TRUE
+
+    # DOWNLOAD_NO_EXTRACT FALSE
+    DOWNLOAD_EXTRACT_TIMESTAMP FALSE
+    EXCLUDE_FROM_ALL
 )
+
 FetchContent_MakeAvailable(soci)
